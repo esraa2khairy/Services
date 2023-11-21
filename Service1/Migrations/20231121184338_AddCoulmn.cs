eccac00204c3919740a5db80bf1fad6c319b55b7
esraa2khairy <esraa20191700093@cis.asu.edu.eg>
2023-11-21 20:52:47 +0200

    Create Tables in Database

---
+++ Service1/Migrations/20231121184338_AddCoulmn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Service1.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCoulmn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Location",
+                table: "Data",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Location",
+                table: "Data");
+        }
+    }
+}
