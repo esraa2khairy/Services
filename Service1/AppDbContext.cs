59bec354411c8cea169efe181fb3b3bc12b35e21
esraa2khairy <esraa20191700093@cis.asu.edu.eg>
2023-11-21 20:55:08 +0200

    Add DbContext

---
+++ Service1/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Service1.Models;
+namespace Service1
+{
+    public class AppDbContext : DbContext
+    {
+        
+            public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+            {
+            }
+           
+            public DbSet<DataSheet> Data { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+        }
+    }
+    
+}
