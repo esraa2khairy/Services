c0222ff28aee2f0589418f37adc0170c4a73dfa7
esraa2khairy <esraa20191700093@cis.asu.edu.eg>
2023-11-21 20:53:13 +0200

    Add Data of sheet Model

---
+++ Service1/Models/DataSheet.cs
@@ -0,0 +1,13 @@
+﻿namespace Service1.Models
+{
+    public class DataSheet
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Location { get; set; } = string.Empty;
+        public double Price { get; set; }
+        public string Color { get; set; } = string.Empty;
+
+    }
+}
